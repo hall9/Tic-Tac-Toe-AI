6944cdfb9864b329c271de0813e177c67a16b9e7
Alexander Hall: Work Mac <Alexander.d.hall@gmail.com>
2012-04-18 22:42:35 -0400

    Primes

--- src/proj4/TicTacToe.java
+++ src/proj4/TicTacToe.java
@@ -5,7 +5,7 @@ public class TicTacToe {
 	/* prime Numbers for Heap Size
 	 * 
 	 *  20011  - if only just above 20k is needed
-	 * 	29303  - If 80% of the heap just always be open
+	 * 	29303  - If 80% of the heap has always be open
 	*/
 	
 	

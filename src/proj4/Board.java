13119cc7820d9cedebc6cb01fb434b93562174a0
Alexander Hall: Work Mac <Alexander.d.hall@gmail.com>
2012-04-19 00:32:30 -0400

    Added the baord members

--- src/proj4/Board.java
+++ src/proj4/Board.java
@@ -2,4 +2,16 @@ package proj4;
 
 public class Board {
 
+	public Board (int state1[][], int wins1, int losses1, double probability1) {
+		
+		state = state1;
+		wins = wins1;
+		losses = losses1;
+		probability = probability1;
+	}
+	
+	public int state[][];
+	public int wins;
+	public int losses;
+	public double probability;
 }
